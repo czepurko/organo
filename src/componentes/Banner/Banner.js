a9e9a0d4cd4a81ae0a6086744aee84d02a4069a5
Marcos <czepurko81@gmail.com>
2024-06-04 07:57:16 -0300

    first commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,15 @@
+import './Banner.css'
+
+function Banner() {
+
+    //jsx
+    return (
+        <header className="banner">
+ <img src="/imagens/banner.png" alt="O banner principal da pagina do Organo" />
+        </header>
+       
+    )
+
+}
+
+export default Banner
